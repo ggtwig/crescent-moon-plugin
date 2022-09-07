83c037ccf2eacad32465e66553ebe608d928b9c9
Jack Waters <61799247+ggtwig@users.noreply.github.com>
2022-09-07 14:46:43 +0100

    added nichirin sword right click event to ItemEvents

--- src/main/java/com/jack/crescentmoon/events/ItemEvents.java
+++ src/main/java/com/jack/crescentmoon/events/ItemEvents.java
@@ -6,18 +6,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class ItemEvents implements Listener {
 
     @EventHandler
     public static void onRightClick(PlayerInteractEvent e) {
 
+        Player player = e.getPlayer();
+
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (e.getItem() != null) {
                 if (e.getItem().getItemMeta().equals(ItemManager.wand.getItemMeta())) {
-                    Player player = e.getPlayer();
                     player.getWorld().createExplosion(player.getLocation(), 2.0f);
                     player.sendMessage("§6You dare use the Stick of Truth?!");
+                } else if (e.getItem().getItemMeta().equals(ItemManager.nichirin.getItemMeta())) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 200, 10));
                 }
             }
         }
