8f13e8d515a9eacfee06eaed9108d69faabd75d4
Jack Waters <61799247+ggtwig@users.noreply.github.com>
2022-09-07 14:49:12 +0100

    created zoroSet + reworked all existing items to use ItemBuilder

--- src/main/java/com/jack/crescentmoon/items/ItemManager.java
+++ src/main/java/com/jack/crescentmoon/items/ItemManager.java
@@ -6,55 +6,50 @@ import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class ItemManager {
 
     public static ItemStack wand;
-    public static ItemStack enma;
-
     public static ItemStack nichirin;
+    public static HashMap<String, ItemStack> zoroSet = new HashMap<>();
 
     public static void init() {
         createWand();
-        createEnma();
+        createZoroSet();
         createNichirin();
     }
 
     private static void createWand() {
-        ItemStack item = new ItemStack(Material.STICK, 1);
-        ItemMeta meta = item.getItemMeta();
-        AttributeModifier modifier = new AttributeModifier("generic.attack_damage", 100, AttributeModifier.Operation.ADD_NUMBER);
-        meta.setDisplayName("§6Stick of Truth");
-        List<String> lore = new ArrayList<>();
-        lore.add("§7This powerful artifact is a relic of");
-        lore.add("§7Minecraft's ancient history!");
-        meta.setLore(lore);
-        meta.addEnchant(Enchantment.LUCK, 1, true);
-        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-        item.setItemMeta(meta);
-        wand = item;
+        wand = new ItemBuilder(Material.STICK, 1)
+                .setDisplayName("§6Stick of Truth")
+                .setLore(
+                        " ",
+                        "§7This powerful artifact is a relic of ",
+                        "§7Minecraft's ancient history!")
+                .addEnchant(Enchantment.LUCK, 1, true)
+                .addAttributeModifier("generic.attack_damage", 100, AttributeModifier.Operation.ADD_NUMBER, Attribute.GENERIC_ATTACK_DAMAGE)
+                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                .addItemFlag(ItemFlag.HIDE_ATTRIBUTES).build();
     }
 
-    private static void createEnma() {
-        ItemStack item = new ItemStack(Material.NETHERITE_SWORD, 1);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName("§8Enma");
-        List<String> lore = new ArrayList<>();
-        lore.add(" ");
-        lore.add("§7Enma is one of the 21 Great Grade swords crafted ");
-        lore.add("§7by Shimotsuki Kozaburo in the Wano Country");
-        meta.setLore(lore);
-        meta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
-        meta.addEnchant(Enchantment.DURABILITY, 10, true);
-        //meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        item.setItemMeta(meta);
-        enma = item;
+    private static void createZoroSet() {
+        ItemStack zoroSword = new ItemBuilder(Material.NETHERITE_SWORD, 1)
+                .setDisplayName("§8Enma")
+                .setLore(
+                        " ",
+                        "§7Enma is one of the 21 Great Grade swords crafted ",
+                        "§7by Shimotsuki Kozaburo in the Wano Country")
+                .addEnchant(Enchantment.DAMAGE_ALL, 10, true)
+                .addEnchant(Enchantment.DURABILITY, 10, true).build();
+
+        ItemStack zoroChestplate = new ItemBuilder(Material.NETHERITE_CHESTPLATE, 1)
+                .setDisplayName("§cZoro Chestplate")
+                .addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10, true).build();
+
+        zoroSet.put("zoroSword" , zoroSword);
+        zoroSet.put("zoroChestplate", zoroChestplate);
     }
 
     private static void createNichirin() {
